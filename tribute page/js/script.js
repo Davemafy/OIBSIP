05bacb74d9c5c1779b6ae16be8443f9911781e31
Davemafy <imafidondavid1@gmail.com>
2024-03-04 13:00:11 +0100

    feat: add video to tribute page

--- tribute page/js/script.js
+++ tribute page/js/script.js
@@ -1,15 +1,14 @@
 const playBtn = document.getElementById("play-btn");
 const video = document.querySelector("video");
 
-playBtn.addEventListener("click", togglePlay);
+playBtn.addEventListener("click", playPause);
 
-function togglePlay() {
+function playPause() {
   if (video.paused) {
-    console.log(playBtn);
-    playBtn.querySelector("img").src = "../icons/play.png";
-    // video.play();
-  } else {
-    // video.pause();
     playBtn.querySelector("img").src = "../icons/pause.png";
+    video.play();
+  } else {
+    video.pause();
+    playBtn.querySelector("img").src = "../icons/play.png";
   }
 }
